8d33144b7f14da18f20c13c0ee709546b56af4f2
Joerg Florin <joerg@casa-blu.de>
2023-05-24 19:41:19 +0200

    intial

---
+++ collectd-fritzbox/docker-build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker build --no-cache -t r0ns3n/collectd-fritzbox:latest .
---
+++ collectd-fritzbox/docker-run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+docker run --network host --rm -i -t --name collectd-fritzbox r0ns3n/collectd-fritzbox:latest
